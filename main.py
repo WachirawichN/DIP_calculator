6c6a8408be7849b7736f6f088bafb97fc247660e
Page <hedgaming93@gmail.com>
2023-12-06 09:58:47 +0700

    change font size

--- main.py
+++ main.py
@@ -2,124 +2,130 @@ import tkinter as tk
 
 class MyCalculator:
     def __init__(self):
+        height = 6
+        width = 12
+        xOffet = 100
+        yOffset = 100
+        fontSize = 10
+
         self.root = tk.Tk()
-        self.root.geometry("200x300")
+        self.root.geometry("400x600")
         self.root.title("Epic gaming calculator")
 
         #Row0
         self.label = tk.Label(self.root, text="whats up Beijing",
                               font=('Comic Sans MS', 10))
-        self.label.place(x=0, y=0)
+        self.label.place(x=xOffet*0, y=yOffset*0)
 
         #Row1
         self.button = tk.Button(self.root, text="C",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=0, y=50)
+                                width=width, height=height)
+        self.button.place(x=xOffet*0, y=yOffset*1)
         self.button = tk.Button(self.root, text="()",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=50, y=50)
+                                width=width, height=height)
+        self.button.place(x=xOffet*1, y=yOffset*1)
         self.button = tk.Button(self.root, text="%",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=100, y=50)
+                                width=width, height=height)
+        self.button.place(x=xOffet*2, y=yOffset*1)
         self.button = tk.Button(self.root, text="/",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=150, y=50)
+                                width=width, height=height)
+        self.button.place(x=xOffet*3, y=yOffset*1)
 
         #Row2
         self.button = tk.Button(self.root, text="7",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=0, y=100)
+                                width=width, height=height)
+        self.button.place(x=xOffet*0, y=yOffset*2)
         self.button = tk.Button(self.root, text="8",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=50, y=100)
+                                width=width, height=height)
+        self.button.place(x=xOffet*1, y=yOffset*2)
         self.button = tk.Button(self.root, text="9",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=100, y=100)
+                                width=width, height=height)
+        self.button.place(x=xOffet*2, y=yOffset*2)
         self.button = tk.Button(self.root, text="*",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=150, y=100)
+                                width=width, height=height)
+        self.button.place(x=xOffet*3, y=yOffset*2)
 
         #Row3
         self.button = tk.Button(self.root, text="4",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=0, y=150)
+                                width=width, height=height)
+        self.button.place(x=xOffet*0, y=yOffset*3)
         self.button = tk.Button(self.root, text="5",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=50, y=150)
+                                width=width, height=height)
+        self.button.place(x=xOffet*1, y=yOffset*3)
         self.button = tk.Button(self.root, text="6",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=100, y=150)
+                                width=width, height=height)
+        self.button.place(x=xOffet*2, y=yOffset*3)
         self.button = tk.Button(self.root, text="-",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=150, y=150)
+                                width=width, height=height)
+        self.button.place(x=xOffet*3, y=yOffset*3)
 
         #Row4
         self.button = tk.Button(self.root, text="1",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=0, y=200)
+                                width=width, height=height)
+        self.button.place(x=xOffet*0, y=yOffset*4)
         self.button = tk.Button(self.root, text="2",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=50, y=200)
+                                width=width, height=height)
+        self.button.place(x=xOffet*1, y=yOffset*4)
         self.button = tk.Button(self.root, text="3",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=100, y=200)
+                                width=width, height=height)
+        self.button.place(x=xOffet*2, y=yOffset*4)
         self.button = tk.Button(self.root, text="+",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=150, y=200)
+                                width=width, height=height)
+        self.button.place(x=xOffet*3, y=yOffset*4)
 
         #Row5
         self.button = tk.Button(self.root, text="+/-",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=0, y=250)
+                                width=width, height=height)
+        self.button.place(x=xOffet*0, y=yOffset*5)
         self.button = tk.Button(self.root, text="0",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=50, y=250)
+                                width=width, height=height)
+        self.button.place(x=xOffet*1, y=yOffset*5)
         self.button = tk.Button(self.root, text=".",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=100, y=250)
+                                width=width, height=height)
+        self.button.place(x=xOffet*2, y=yOffset*5)
         self.button = tk.Button(self.root, text="=",
-                                font=('Arial', 4),
+                                font=('Arial', fontSize),
                                 justify="center",
-                                width=15, height=10)
-        self.button.place(x=150, y=250)
+                                width=width, height=height)
+        self.button.place(x=xOffet*3, y=yOffset*5)
 
         self.root.mainloop()
 
